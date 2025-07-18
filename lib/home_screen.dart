6c67297b1189c6c2f29423ab2f034a58cd471930
backbencherstudio <shmrawnak@gmail.com>
2025-07-18 10:29:42 +0600

    add counter and switch functionality with riverpod

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,50 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// // provider
-// final helloProvider = Provider<String>((ref) {
-//   return 'Hello';
-// });
-//
-// class HomeScreen extends ConsumerWidget { // ConsumerWidget is a widget that is used as the replacement of statelessWidget and can access providers and rebuilds when the provider changes
-//   const HomeScreen({super.key});
-//
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) { // WidgetRef is a reference to the widget. For this we don't need to use context
-//     final hello = ref.watch(helloProvider); // watch is a function that returns the value of the provider and updates the widget when the value changes
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Home'),
-//       ),
-//       body: Center(
-//         child: Text(hello, style: const TextStyle(fontSize: 24)), // The value of the provider is passed to the Text widget
-//       ),
-//     );
-//   }
-// }
-
-
-final ageProvider = Provider<int>((ref) {
-  return 20;
+final counter = StateProvider<int>((ref) { /// To manage small state changes StateProvider is used
+  return 0;
+});
+final switchProvider = StateProvider<bool>((ref) {
+  return false;
 });
 
-class HomeScreen extends ConsumerStatefulWidget { // ConsumerStatefulWidget is a widget that is used as the replacement of statefulWidget and can access providers and rebuilds when the provider state changes
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  ConsumerState<HomeScreen> createState() => _HomeScreenState(); // ConsumerState is a state that is used as the replacement of state
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
-  Widget build(BuildContext context) { // Here we don't need to use WidgetRef cause we are using ConsumerState and ConsumerStatefulWidget and WidgetRef is coming from these
-    final age = ref.watch(ageProvider); // watch is a function that returns the value of the provider and updates the widget when the value changes
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
       body: Center(
-        child: Text(age.toString(), style: const TextStyle(fontSize: 24)), // The value of the provider is passed to the Text widget
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Consumer( /// To define which widget should be change when the state changes
+              builder: (context, ref, child) {
+                final age = ref.watch(counter);
+                return Text(age.toString(), style: const TextStyle(fontSize: 24)); /// Here only the text will change and rebuild
+              }
+            ),
+            SizedBox(height: 32),
+            Consumer(
+              builder: (context, ref, child) {
+                final switchValue = ref.watch(switchProvider);
+                return Switch(
+                  value: switchValue,
+                  onChanged: (value) {
+                    ref.read(switchProvider.notifier).state = value;
+                  },
+                );
+              },
+            ),
+            SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    ref.read(counter.notifier).state--; /// state will be changed and value of the provider will be decreased
+                  },
+                  child: const Icon(Icons.remove),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    ref.read(counter.notifier).state++; /// state will be changed and value of the provider will be increased
+                  },
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
