a6cb7fcfb8c910d2753c61aa31b042165babef52
backbencherstudio <shmrawnak@gmail.com>
2025-07-19 09:07:26 +0600

    implement todo app

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,104 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_basic/search_provider.dart';
-import 'package:riverpod_basic/temp/validators.dart';
 
-class HomeScreen extends ConsumerStatefulWidget {
+import 'item_provider.dart';
+
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  ConsumerState<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends ConsumerState<HomeScreen> {
-  final TextEditingController searchController = TextEditingController();
-  final FocusNode searchFocusNode = FocusNode();
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
-  @override
-  void dispose() {
-    super.dispose();
-    searchController.dispose();
-    searchFocusNode.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final item = ref.watch(itemProvider);
     return Scaffold(
-      backgroundColor: Colors.deepPurple,
-      appBar: AppBar(title: const Text('Home')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Form(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                key: formKey,
-                child: Stack(
-                  alignment: Alignment.topCenter,
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 54,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey),
-                        borderRadius: BorderRadius.circular(8.0),
-                        color: Colors.white,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top:1.0),
-                      child: Container(
-                        alignment: Alignment.topCenter,
-                        width: MediaQuery.of(context).size.width * 0.91,
-                        padding: const EdgeInsets.fromLTRB(1, 1, 1, 4),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8.0),
-                          color: Colors.transparent,
-                        ),
-                        child: TextFormField(
-                          focusNode: searchFocusNode,
-                          onChanged: (value) {
-                            ref.read(searchProvider.notifier).setSearch(value);
-                          },
-                          controller: searchController,
-                          decoration: const InputDecoration(
-                            filled: true,
-                            fillColor: Color(0xFFD3C3F4),
-                            contentPadding: EdgeInsets.all(8.0),
-                            isDense: false,
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(8.0),
-                              ),
-                            ),
-                            hintText: 'Search',
-                            errorStyle: TextStyle(
-                              color: Colors.red,
-                              fontSize: 12.0,
-                              fontWeight: FontWeight.bold
-                            )
-                          ),
-                          validator: InputValidators.emailValidator,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 20),
-              Consumer(
-                builder: (context, ref, child) {
-                  final searchText = ref.watch(searchProvider);
-                  return Text(searchText.query);
-                },
-              ),
-            ],
-          ),
-        ),
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          ref.read(itemProvider.notifier).addItem('New Item');
+        },
+        child: Icon(Icons.add),
+      ),
+      body: item.isEmpty ? Center(child: Text('No Items'),) : ListView.builder(
+        itemCount: item.length,
+        itemBuilder: (context, index) {
+          final itemDetails = item[index];
+          return ListTile(
+            title: Text(itemDetails.title),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(onPressed: (){
+                  ref.read(itemProvider.notifier).updateItem(itemDetails.id, 'new');
+                }, icon: Icon(Icons.edit)),
+                IconButton(onPressed: (){
+                  ref.read(itemProvider.notifier).deleteItem(itemDetails.id);
+                }, icon: Icon(Icons.delete)),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
---
+++ lib/item_model.dart
@@ -0,0 +1,9 @@
+class ItemModel {
+  String id;
+  String title;
+
+  ItemModel({
+    required this.id,
+    required this.title,
+  });
+}
